73ad82d70c64967f74cdf9e423c96864c1ece7e5
Dave Syer <dsyer@vmware.com>
2023-07-02 12:49:45 +0100

    Works but doesn't use custom module

--- src/main/java/com/example/ExampleMojo.java
+++ src/main/java/com/example/ExampleMojo.java
@@ -2,13 +2,14 @@ package com.example;
 
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.inject.Singleton;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+
+import com.google.inject.AbstractModule;
 
 @Mojo(name = "run", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class ExampleMojo extends AbstractMojo {
@@ -31,9 +32,19 @@ interface Service {
 }
 
 @Named
-@Singleton
 class DefaultService implements Service {
 	public void say() {
 		System.err.println("Hi");
+	}
+}
+
+@Named
+class MyModule extends AbstractModule {
+	public MyModule() {
+		System.err.println("MyModule");
+	}
+	@Override
+	protected void configure() {
+		bind(Service.class).to(DefaultService.class);
 	}
 }
